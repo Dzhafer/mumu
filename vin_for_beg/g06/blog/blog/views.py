26045a9f25d0c80963884dd1c3ce62f822dfc8eb
Dzhafer <dzeparovdzafer@gmail.com>
2021-06-27 14:22:40 +0300

    add g06+

--- vin_for_beg/g06/blog/blog/views.py
+++ vin_for_beg/g06/blog/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+
+from django.urls import reverse_lazy
+
 
 class BlogListView(ListView):
     model = Post
@@ -16,4 +19,14 @@ class BlogCreateView(CreateView): # new
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
